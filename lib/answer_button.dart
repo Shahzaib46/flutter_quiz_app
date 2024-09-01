f9ff8e239072c9e0dfb22edd4df35e447c3f4503
Shahzaib Shahid <shahidshahzaib806@gmail.com>
2024-09-02 01:00:00 +0500

    Creating Questions Screen userinterface and creating custom answer button widget

---
+++ lib/answer_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget{
+  const AnswerButton({required this.text, required this.onTap,super.key});
+  
+  final String text;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ElevatedButton(
+      onPressed: onTap, 
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+        backgroundColor: const Color.fromARGB(255, 3, 42, 109),
+        foregroundColor: Colors.white
+      ),
+      child: Text(text)
+      );
+
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget{
 
@@ -13,6 +14,22 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(context) {
-    return const Text("Questions");
+    return SizedBox(
+      width: double.infinity, // double.infinity is built in value which takes complete width
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Question is here ...", style: TextStyle(
+            color: Colors.white,
+            fontSize: 20
+          )),
+          const SizedBox(height: 30),
+          AnswerButton(text: "Answer 1", onTap: (){}),
+          AnswerButton(text: "Answer 2", onTap: (){}),
+          AnswerButton(text: "Answer 3", onTap: (){}),
+          AnswerButton(text: "Answer 4", onTap: (){}),
+        ],
+      ),
+    );
   }
 }
