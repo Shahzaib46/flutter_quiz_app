9c826632f348474c34914cfb797aa666e75d172b
Shazy <shahidshahzaib806@gmail.cm>
2024-09-02 17:35:50 +0500

    showing questions data and answers dynamically using map function of lists, setting some alignments for questions screen ui

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/answer_button.dart';
+import 'package:flutter_quiz_app/Data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget{
 
@@ -12,23 +13,31 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen>{
+
+  final currentQuestion = questions[0];
   @override
   Widget build(context) {
     return SizedBox(
       width: double.infinity, // double.infinity is built in value which takes complete width
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Question is here ...", style: TextStyle(
-            color: Colors.white,
-            fontSize: 20
-          )),
-          const SizedBox(height: 30),
-          AnswerButton(text: "Answer 1", onTap: (){}),
-          AnswerButton(text: "Answer 2", onTap: (){}),
-          AnswerButton(text: "Answer 3", onTap: (){}),
-          AnswerButton(text: "Answer 4", onTap: (){}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(currentQuestion.question,
+             style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20
+            ),
+            textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((option){
+              return AnswerButton(text: option, onTap: (){});
+            })
+          ],
+        ),
       ),
     );
   }
